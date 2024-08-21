326fb685b31d8b354b13363107a66c06159653ba
rlmgtr <rlmagtira@gmail.com>
2024-08-21 15:02:23 +0800

    update

---
+++ readline.js
@@ -0,0 +1,10 @@
+import readline from "readline";
+
+const rl = readline.createInterface({
+input: process.stdin,
+output:process.stdout,
+});
+
+rl.question("What is your name?", (name) => {
+    console.log(`Hello, ${name}!`);
+});
