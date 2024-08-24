b7d3d3dff6b03066db66c5079f707ca4672e6039
rlmgtr <rlmagtira@gmail.com>
2024-08-24 14:38:10 +0800

    updateq

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+// expres function is used to initiaye an express backend application
+// 
+
+const app =  express();
+
+
+
+
+app.listen()
+
+
+continue at 27:22
--- nexttick.js
+++
@@ -1,5 +0,0 @@
-process.nextTick(function() {
-    console.log('NextTick callback');
-  });
-
-  console.log("after nextTick")
