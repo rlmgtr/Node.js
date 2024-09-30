a168838d0a46efe075a19a0a142941b98552451f
rlmgtr <rlmagtira@gmail.com>
2024-09-30 11:43:20 +0800

    update
    update

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-import express from "express";
-
-// expres function is used to initiaye an express backend application
-// 
-
-const app =  express();
-
-
-
-
-app.listen()
+import express from "express"; 
+// express function call is used to initialize an express backend application
+// we are creating an express instance and assigning it to the app constant
+const app = express();
+// takes two parameters
+// first number is port number
+// second number is the callback function
+// where we can indicate the code that will run once the server is successfully started.
 
 
-continue at 27:22
+
+app.listen(3000, () => {
+console.log("server is running on port 3000");
+
+});
