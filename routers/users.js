cabdebb3b196642b6069293cb7e691cbf7719d1d
rlmgtr <rlmagtira@gmail.com>
2024-10-08 14:27:08 +0800

    update

--- routers/users.js
+++ routers/users.js
@@ -1,22 +1,24 @@
 import express from "express";
 const userRouter = express.Router();
 
-
-userRouter.get("/", (req, res)=> {
-    res.send("User Profile Main page")
-    });
-
-userRouter.get("/profile", (req, res)=> {
-res.send("User Profile page")
+// Main user page
+userRouter.get("/", (req, res) => {
+    res.send("User Profile Main page");
 });
 
+// User profile page
+userRouter.get("/profile", (req, res) => {
+    res.send("User Profile page");
+});
+
+// Update user profile
 userRouter.put("/profile", (req, res) => {
-    res.send("Update User Profile page")
+    res.send("Update User Profile page");
 });
 
+// Delete user profile
 userRouter.delete("/profile", (req, res) => {
-    res.send("delete User Profile page")
+    res.send("Delete User Profile page");
 });
 
-
-export default userRouter
+export default userRouter;
